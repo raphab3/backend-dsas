77087ac69b4fcb0b9fc98dcf646b79b15ecff3de
Rafael Batista - AION <raphab33@hotmail.com>
2024-01-22 05:31:54 -0300

    Remove unused code and console.log in FindExternalSigpmpbService

--- src/modules/persosnSig/services/findExternal.sigpmpb.service.ts
+++ src/modules/persosnSig/services/findExternal.sigpmpb.service.ts
@@ -4,8 +4,6 @@ import env from '@config/env';
 
 @Injectable()
 export class FindExternalSigpmpbService {
-  // constructor(private readonly personSigRepository: PersonSigRepository) {}
-
   async execute(matricula: string): Promise<any> {
     const config = {
       method: 'get',
@@ -14,18 +12,13 @@ export class FindExternalSigpmpbService {
         'Content-Type': 'application/json',
         Authorization: `${env.TOKEN_SIGPMPB}`,
         Referer: env.Referer_SIGPMPB,
-        'Access-Control-Allow-Origin': '*',
-        Accept: '*',
-        'Sec-CH-UA-Platform': '"Linux"',
-        'User-Agent':
-          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       },
     };
 
     try {
       axios(config)
         .then(function (response) {
-          console.log(response);
+          console.log(response.data);
           return response.data;
         })
         .catch(function (error) {
