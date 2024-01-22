1f512b029e2cd3a6859906f84f9ed5d8f9a2a57f
Rafael Batista - AION <raphab33@hotmail.com>
2024-01-22 10:30:01 -0300

    Refactor findExternalSigpmpbService to use axios for API request

--- src/modules/persosnSig/services/findExternal.sigpmpb.service.ts
+++ src/modules/persosnSig/services/findExternal.sigpmpb.service.ts
@@ -1,41 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import env from '@config/env';
-
+import axios, { AxiosRequestConfig } from 'axios';
 @Injectable()
 export class FindExternalSigpmpbService {
   async execute(matricula: string): Promise<any> {
+    const config: AxiosRequestConfig<any> = {
+      headers: {
+        Authorization: `${env.TOKEN_SIGPMPB}`,
+        referer: env.Referer_SIGPMPB,
+      },
+    };
+
     try {
-      const response = await fetch(
+      const response = await axios(
         `${env.API_SIGPMPB}/servidores/${matricula}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `${env.TOKEN_SIGPMPB}`,
-            referer: env.Referer_SIGPMPB,
-          },
-        },
+        config,
       );
 
-      console.log(response.headers);
+      console.log('status: ', response.status);
+      console.log('statusText: ', response.statusText);
+      console.log('headers: ', response.headers);
+      console.log('config: ', response.config);
+      console.log('data: ', response.data);
 
-      console.log(response);
-
-      if (!response.ok) {
-        throw new Error(`Erro na API: ${response.status}`);
-      }
-
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.includes('application/json')) {
-        const data = await response.json();
-        console.log(data);
-        return data;
-      } else {
-        // Tratamento para respostas que não são JSON
-        const textData = await response.text();
-        console.log(textData);
-        return textData;
-      }
+      return response.data;
     } catch (error) {
       console.error('Erro ao consultar a API externa', error);
       throw error;
