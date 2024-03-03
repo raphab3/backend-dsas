38be57c043f29cce3826a4f4aefff26d3475d3bd
Rafael Batista <raphab33@hotmail.com>
2024-03-03 08:35:08 -0300

    Refactor GenerateRolesService to remove unused code and improve performance

--- src/shared/seeders/services/GenerateRoles.service.ts
+++ src/shared/seeders/services/GenerateRoles.service.ts
@@ -1,6 +1,5 @@
 import { groupsOfPermissions } from '@modules/permissions/interfaces/permissionsEnum';
 import { Injectable } from '@nestjs/common';
-import { Permission } from '@modules/permissions/typeorm/entities/permission.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Role } from '@modules/roles/typeorm/entities/role.entity';
@@ -8,41 +7,31 @@ import { Role } from '@modules/roles/typeorm/entities/role.entity';
 @Injectable()
 export class GenerateRolesService {
   constructor(
-    @InjectRepository(Permission)
-    private readonly permissionRepository: Repository<Permission>,
     @InjectRepository(Role)
     private readonly roleRepository: Repository<Role>,
   ) {}
 
   async execute(): Promise<void> {
     Object.keys(groupsOfPermissions).map(async (role) => {
-      const permissionsNames = groupsOfPermissions[role];
-
-      const findPermissions = await this.permissionRepository
-        .createQueryBuilder('permissions')
-        .where('permissions.name IN (:...permissionsNames)', {
-          permissionsNames,
-        })
-        .getMany();
+      console.log('role selected', role);
 
       const roleExists = await this.roleRepository.findOne({
         where: { name: role },
       });
 
-      console.log(roleExists);
+      console.log('roleExists', !!roleExists);
 
       if (!roleExists) {
-        const roleSaved = await this.roleRepository.create({
+        const roleSaved = this.roleRepository.create({
           name: role,
           description: `Grupo de permissões para ${role}`,
         });
 
-        const roleCreated = await this.roleRepository.save(roleSaved);
-
-        await this.roleRepository.update(roleCreated.id, {
-          permissions: findPermissions,
-        });
+        await this.roleRepository.save(roleSaved);
+        console.log('role saved', !!roleSaved);
       }
+
+      console.log('role created');
     });
 
     console.log('Finished roles generation');
