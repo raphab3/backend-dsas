acf6bd997405454432f19c8664c29261ee65335b
Rafael Batista <raphab33@hotmail.com>
2025-04-08 18:50:57 -0300

    refactor: remove ConfigModule from providers in ProvidersModule

--- src/shared/providers/providers.module.ts
+++ src/shared/providers/providers.module.ts
@@ -2,14 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import CryptoHashProvider from './HashProvider/implementations/CryptoHashProvider';
 import AuditModule from '@modules/audits/Audit.module';
 import { S3Provider } from './StorageProvider/services/S3StorageProvider';
-import { ConfigModule } from '@nestjs/config';
 
 const HashProvider = {
   provide: 'HashProvider',
   useClass: CryptoHashProvider,
 };
 
-const providers = [ConfigModule, HashProvider, S3Provider];
+const providers = [HashProvider, S3Provider];
 
 const forwardRefModules = [forwardRef(() => AuditModule)];
 
